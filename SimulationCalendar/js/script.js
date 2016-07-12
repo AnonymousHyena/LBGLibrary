3eaf61b4ae2445b528ea9e6aa0d9d80e7acb3809
Στάθης Μποζίκας <stathisbozikas@MacBook-Air-2.local>
2016-07-12 19:01:42 +0300

    simulation time correction

--- SimulationCalendar/js/script.js
+++ SimulationCalendar/js/script.js
@@ -1,4 +1,5 @@
 //1468334719356 17:45 -> 1/10
+//
 var day=1;
 var month=10;
 var oneDay = 120000;
@@ -7,13 +8,13 @@ var date = new Date();
 var t = date.getTime()-1468334719356;
 t=t/(1000*60); //in minutes
 t=t/2; // in simulation days
-t=parseInt(t);
+t=parseInt(t)-7;
 console.log(t);
 
 var temp=t/30;//
 month =month + parseInt(temp);
 day=day+(t%30);
-day=day-3; //xathike sti poreia
+day=day; //xathike sti poreia
 document.addEventListener("DOMContentLoaded",function (){
 document.getElementById("time").innerHTML=day+"/"+month;
 setInterval(incDay, oneDay);
